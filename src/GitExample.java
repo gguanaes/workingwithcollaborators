dae861412601073d2d6b67189a1fbaa88916dc18
mmeliuk <meliukm@seas.upenn.edu>
2019-04-14 09:29:08 -0700

    Updated line number 2

--- src/GitExample.java
+++ src/GitExample.java
@@ -3,7 +3,7 @@ public class GitExample {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
-		System.out.println("hello git 2");
+		System.out.println("hello git 2, Masha updated this line.");
 		System.out.println("This is a new line from Masha.");
 		
 	}
