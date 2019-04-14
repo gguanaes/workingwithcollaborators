cebf325e428e50a742f69a1e30e1989c7c94ddd4
mmeliuk <meliukm@seas.upenn.edu>
2019-04-14 09:21:00 -0700

    Added two words to line 3

--- src/GitExample.java
+++ src/GitExample.java
@@ -4,7 +4,7 @@ public class GitExample {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
 		System.out.println("hello git 2");
-		System.out.println("This is a new line");
+		System.out.println("This is a new line from Masha");
 		
 	}
 	
