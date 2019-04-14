c345af79dac4cbc1d20b6dbfd6542f4a20d00154
mmeliuk <meliukm@seas.upenn.edu>
2019-04-14 09:19:06 -0700

    Added a new line to print

--- src/GitExample.java
+++ src/GitExample.java
@@ -4,6 +4,8 @@ public class GitExample {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
 		System.out.println("hello git 2");
+		System.out.println("This is a new line");
+		
 	}
 	
 }
